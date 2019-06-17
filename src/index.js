01f45f1b111c567dfdc17d283627d0ec4e35753d
Dabkdn <nkh1111202@gmail.com>
2019-06-17 11:34:14 +0700

    up down number input

--- src/index.js
+++ src/index.js
@@ -4,15 +4,54 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
+class ChangeNumber extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {a: ''};
+
+    // This binding is necessary to make `this` work in the callback
+    this.up = this.up.bind(this);
+    this.down = this.down.bind(this);
+    this.updateInput = this.updateInput.bind(this);
   }
-  
-  const element = <Welcome name="Dabk" />;
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
+
+  up() {
+    this.setState(state => ({
+      a: state.a+1
+    }));
+  }
+
+  down() {
+    this.setState(state => ({
+      a: state.a-1
+    }));
+  }
+
+  updateInput(evt) {
+    this.setState({
+      a: parseInt(evt.target.value)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.down}>
+          -
+        </button>
+        <input type="text" name="number" value={this.state.a} onChange={this.updateInput}/>
+        <button onClick={this.up}>
+          +
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ChangeNumber />,
+  document.getElementById('root')
+);
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
