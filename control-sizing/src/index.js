5eb557b231bb646ab0ef84c39de61162343456dc
nguyentungduc <nguyentungducbk@gmail.com>
2019-06-18 14:51:33 +0700

    [control-sizing]--fix bug

--- control-sizing/src/index.js
+++ control-sizing/src/index.js
@@ -6,36 +6,42 @@ import './index.css';
 class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: 4.5};
+    this.state = {value: 4};
   }
 
+  onTodoChange(value){
+    value = isNaN(parseInt(value)) ? 0 : parseInt(value);
+    this.setState({
+      value: value
+    });
+  }
+  
   down() {
     this.setState(state => ({
-      value: state.value -1
+      value: parseInt(state.value) - 1
     }));
   }
 
   up() {
     this.setState(state => ({
-      value: state.value + 1
+      value: parseInt(state.value) + 1
     }));
   }
 
-  render() {
-  
+  render() {  
     return (
-      <div class="form-group">
+      <div className="form-group">
         <label>Quantity: </label>
-        <div class="input-group">
-            <div class="input-group-btn">
-              <button id="down" class="btn btn-default" onClick={()=>this.down(this.state.value)}>
-                <span class="glyphicon glyphicon-minus"></span>
+        <div className="input-group">
+            <div className="input-group-btn">
+              <button id="down" className="btn btn-default" onClick={()=>this.down(this.state.value)}>
+                <span className="glyphicon glyphicon-minus"></span>
               </button>
             </div>
-            <input type="text" id="myNumber" class="form-control input-number" value={this.state.value} />
-            <div class="input-group-btn">
-              <button id="up" class="btn btn-default" onClick={()=>this.up(this.state.value)}>
-                <span class="glyphicon glyphicon-plus"></span>
+            <input type="text" id="myNumber" className="form-control input-number" onChange={(e) => this.onTodoChange(e.target.value)} value={this.state.value} />
+            <div className="input-group-btn">
+              <button id="up" className="btn btn-default" onClick={()=>this.up(this.state.value)}>
+                <span className="glyphicon glyphicon-plus"></span>
               </button>
             </div>
         </div>
