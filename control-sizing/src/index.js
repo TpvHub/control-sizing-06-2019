e4b9623d76a4cba7e70770e87ad287a62a7cd058
nguyentungduc <nguyentungducbk@gmail.com>
2019-06-18 08:04:26 +0700

    State control-sizing

--- control-sizing/src/index.js
+++ control-sizing/src/index.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 4.5};
+  }
+
+  down() {
+    this.setState(state => ({
+      value: state.value -1
+    }));
+  }
+
+  up() {
+    this.setState(state => ({
+      value: state.value + 1
+    }));
+  }
+
+  render() {
+  
+    return (
+      <div class="form-group">
+        <label>Quantity: </label>
+        <div class="input-group">
+            <div class="input-group-btn">
+              <button id="down" class="btn btn-default" onClick={()=>this.down(this.state.value)}>
+                <span class="glyphicon glyphicon-minus"></span>
+              </button>
+            </div>
+            <input type="text" id="myNumber" class="form-control input-number" value={this.state.value} />
+            <div class="input-group-btn">
+              <button id="up" class="btn btn-default" onClick={()=>this.up(this.state.value)}>
+                <span class="glyphicon glyphicon-plus"></span>
+              </button>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <App />,
+  <Toggle />,
   document.getElementById('root')
 );
