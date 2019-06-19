c8bd917ecc4d8e85225224c8ec604735763358d3
nguyentungduc <nguyentungducbk@gmail.com>
2019-06-19 13:27:31 +0700

    [control-sizing] bai2 fix bug

--- control-sizing/src/index.js
+++ control-sizing/src/index.js
@@ -19,57 +19,79 @@ class Toggle extends React.Component {
     for(let i = 0; i < value; i++) {
       this.state.arr.push(0);
     }
-    this.setState({
+    this.setState(state => ({
       value: value,
-    });
+    }));
   }
   
   down(value_down) {
-    this.state.arr = [];
+    let arrNew = [];
     let down = parseInt(value_down) - 1;
-    if(value_down > 0){
+    let arrOld = this.state.arr;
+    if(down > 0){
       for(let i = 0; i < down; i++) {
-        this.state.arr.push(0);
+        arrNew[i] = arrOld[i];
       }
       this.setState(() => ({
         value: down,
+        arr:arrNew
       }));
     }else{
       this.setState(state => ({
         value: 0,
+        arr:arrNew
       }));
     }
   }
 
   up(value_up) {
     let up = parseInt(value_up) + 1;
-    this.setState(() => ({
-      value: up,
-    }));
-    this.state.arr = [];
+    let arrNew = [];
+    let arrOld = this.state.arr;
     for(let i = 0; i < up; i++) {
-      this.state.arr.push(0);
+      if(i>=arrOld.length){
+        arrNew.push(0);
+      }else{
+        arrNew[i] = arrOld[i];
+      }
     }
-    console.log(this.state.arr);
+    this.setState(state => ({
+      value: up,
+      arr:arrNew
+    }));
   }
 
-  resetDown(index){
+  resetDown(index,item){
     index = parseInt(index);
+    item = parseInt(item);
     let arrNew = this.state.arr;
-    arrNew[index] = 0;
+    arrNew[index] = item - index - 1;
     this.setState(() => ({
       arr: arrNew,
-    }));
+    })); 
   }
 
-  setUp(index){
+  setUp(index,item){
     index = parseInt(index);
+    item = parseInt(item);
     let arrNew = this.state.arr;
-    arrNew[index] = index + 1;
+    arrNew[index] = index + item + 1;
+    console.log(arrNew);
     this.setState(() => ({
       arr: arrNew,
     }));    
   }
+
+  onTodoChangeItem(value,index){
+    value = (isNaN(parseInt(value)) || parseInt(value) < 0) ? 0 : parseInt(value);  
+    index = parseInt(index);
+    let arrNew = this.state.arr;
+    arrNew[index] = value;
+    this.setState(() => ({
+      arr: arrNew,
+    })); 
+  }
+  
 
   render() {  
     return (
@@ -98,13 +120,13 @@ class Toggle extends React.Component {
             <div className="form-group">
               <div className="input-group">
                   <div className="input-group-btn">
-                    <button id="down" className="btn btn-default" onClick={()=>this.resetDown(index)}>
+                    <button id="down" className="btn btn-default" onClick={()=>this.resetDown(index,item)}>
                       <span className="glyphicon glyphicon-minus"></span>
                     </button>
                   </div>
-                  <input type="text" id="myNumber" className="form-control input-number" value={item} />
+                  <input type="text" id="myNumber" className="form-control input-number" value={item} onChange={(e) => this.onTodoChangeItem(e.target.value,index)}/>
                   <div className="input-group-btn">
-                    <button id="up" className="btn btn-default" onClick={()=>this.setUp(index)}>
+                    <button id="up" className="btn btn-default" onClick={()=>this.setUp(index,item)}>
                       <span className="glyphicon glyphicon-plus"></span>
                     </button>
                   </div>
