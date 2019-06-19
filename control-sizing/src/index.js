f1f2bf246989ae1820ac3dfba1e4ccb5a1cec12f
nguyentungduc <nguyentungducbk@gmail.com>
2019-06-19 11:14:26 +0700

    [control-size]--bai2 show list compoment and event

--- control-sizing/src/index.js
+++ control-sizing/src/index.js
@@ -6,44 +6,112 @@ import './index.css';
 class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: 4};
+    this.state = {
+      value: 0,
+      arr: []
+    };
   }
 
+
   onTodoChange(value){
-    value = isNaN(parseInt(value)) ? 0 : parseInt(value);
+    value = (isNaN(parseInt(value)) || parseInt(value) < 0) ? 0 : parseInt(value);  
+    this.state.arr = [];
+    for(let i = 0; i < value; i++) {
+      this.state.arr.push(0);
+    }
     this.setState({
-      value: value
+      value: value,
     });
   }
   
-  down() {
-    this.setState(state => ({
-      value: parseInt(state.value) - 1
+  down(value_down) {
+    this.state.arr = [];
+    let down = parseInt(value_down) - 1;
+    if(value_down > 0){
+      for(let i = 0; i < down; i++) {
+        this.state.arr.push(0);
+      }
+      this.setState(() => ({
+        value: down,
+      }));
+    }else{
+      this.setState(state => ({
+        value: 0,
+      }));
+    }
+  }
+
+  up(value_up) {
+    let up = parseInt(value_up) + 1;
+    this.setState(() => ({
+      value: up,
+    }));
+    this.state.arr = [];
+    for(let i = 0; i < up; i++) {
+      this.state.arr.push(0);
+    }
+    console.log(this.state.arr);
+  }
+
+  resetDown(index){
+    index = parseInt(index);
+    let arrNew = this.state.arr;
+    arrNew[index] = 0;
+    this.setState(() => ({
+      arr: arrNew,
     }));
   }
 
-  up() {
-    this.setState(state => ({
-      value: parseInt(state.value) + 1
-    }));
+  setUp(index){
+    index = parseInt(index);
+    let arrNew = this.state.arr;
+    arrNew[index] = index + 1;
+    this.setState(() => ({
+      arr: arrNew,
+    }));    
   }
 
   render() {  
     return (
-      <div className="form-group">
-        <label>Quantity: </label>
-        <div className="input-group">
-            <div className="input-group-btn">
-              <button id="down" className="btn btn-default" onClick={()=>this.down(this.state.value)}>
-                <span className="glyphicon glyphicon-minus"></span>
-              </button>
+      <div className="container">
+        <div className="col-xs-6 col-sm-6 col-md-8 col-lg-6">
+          <div className="form-group">
+            <label>Quantity: </label>
+            <div className="input-group">
+                <div className="input-group-btn">
+                  <button id="down" className="btn btn-default" onClick={()=>this.down(this.state.value)}>
+                    <span className="glyphicon glyphicon-minus"></span>
+                  </button>
+                </div>
+                <input type="text" id="myNumber" className="form-control input-number" onChange={(e) => this.onTodoChange(e.target.value)} value={this.state.value} />
+                <div className="input-group-btn">
+                  <button id="up" className="btn btn-default" onClick={()=>this.up(this.state.value)}>
+                    <span className="glyphicon glyphicon-plus"></span>
+                  </button>
+                </div>
             </div>
-            <input type="text" id="myNumber" className="form-control input-number" onChange={(e) => this.onTodoChange(e.target.value)} value={this.state.value} />
-            <div className="input-group-btn">
-              <button id="up" className="btn btn-default" onClick={()=>this.up(this.state.value)}>
-                <span className="glyphicon glyphicon-plus"></span>
-              </button>
-            </div>
+          </div>
+        </div>
+        <div className="col-xs-6 col-sm-6 col-md-8 col-lg-6"> 
+        {
+          this.state.arr.map((item, index) => (
+            <div className="form-group">
+              <div className="input-group">
+                  <div className="input-group-btn">
+                    <button id="down" className="btn btn-default" onClick={()=>this.resetDown(index)}>
+                      <span className="glyphicon glyphicon-minus"></span>
+                    </button>
+                  </div>
+                  <input type="text" id="myNumber" className="form-control input-number" value={item} />
+                  <div className="input-group-btn">
+                    <button id="up" className="btn btn-default" onClick={()=>this.setUp(index)}>
+                      <span className="glyphicon glyphicon-plus"></span>
+                    </button>
+                  </div>
+              </div>
+            </div> 
+          ))
+        }
         </div>
       </div>
     );
