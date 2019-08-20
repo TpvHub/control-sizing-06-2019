9358b1d6e9c3dd73d9002492b46a4f0a71fb2990
Your Name <ngockhanh.2197@gmail.com>
2019-08-20 15:39:20 +0700

    fix bug

--- src/app.js
+++ src/app.js
@@ -1,56 +1,56 @@
 import React from "react";
-import './app.css'
+import "./app.css";
 
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        number: 50
-
-      };
-    }
-  
-    IncrementItem = () => {
-        this.setState(prevState => {
-          if(prevState.number >=0) {
-            return {
-              number: Number( prevState.number) + 1
-            }
-          } else {
-            return null;
-          }
-        });
-    }
-    DecreaseItem = () => {
-      this.setState(prevState => {
-        if(prevState.number > 0) {
-          return {
-            number: prevState.number - 1
-          }
-        } else {
-          return null;
-        }
-      });
-    }
- 
-    handleChange = (event) => {
-      // if(parseInt(event.target.value)==="NaN")
-      // console.log("q")
-      // if(parseInt(event.target.value)!=='NaN')
-      // console.log(  parseInt(event.target.value))
-      this.setState({number:(event.target.value) });
-    }
-  
-    render() {
-      return ( 
-      <div>
-        <button className="Add" onClick={this.IncrementItem}>+</button>
-        <input className="input_number" type='number'  value={this.state.number} onChange={this.handleChange}/>
-        <button className="Sub" onClick = {this.DecreaseItem}>-</button>
-      </div>
-     
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 50
+    };
   }
+
+  increaseItem = () => {
+    this.setState(prevState => {
+      if (prevState.number >= 0) {
+        return {
+          number: prevState.number + 1
+        };
+      } else {
+        return null;
+      }
+    });
+  };
   
-  export default App;
+  decreaseItem = () => {
+    this.setState(prevState => {
+      if (prevState.number > 0) {
+        return {
+          number: prevState.number - 1
+        };
+      } else {
+        return null;
+      }
+    });
+  };
+
+  handleChange = event => {
+    this.setState({ number: Number(event.target.value) });
+  };
+
+  render() {
+    return (
+      <div>
+        <button className="add" onClick={this.increaseItem}>+</button>
+        <input
+          className="input_number"
+          type="number"
+          value={this.state.number}
+          onChange={this.handleChange}
+        />
+        <button className="sub" onClick={this.decreaseItem}>-</button>
+      </div>
+    );
+  }
+}
+
+export default App;
